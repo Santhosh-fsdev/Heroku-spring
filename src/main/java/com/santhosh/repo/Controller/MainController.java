423ad8afdb1f704e79274ca783ebc2c8b894160f
Santhosh kuamr <santhoshfsdev@gmail.com>
2020-06-05 08:40:31 -0400

    Cross origin change

--- src/main/java/com/santhosh/repo/Controller/MainController.java
+++ src/main/java/com/santhosh/repo/Controller/MainController.java
@@ -3,6 +3,7 @@ package com.santhosh.repo.Controller;
 import com.santhosh.repo.model.Data;
 import com.santhosh.repo.repository.DataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.List;
 import com.santhosh.repo.model.Data;
 
 @RestController
+@CrossOrigin
 public class MainController {
 
 
@@ -23,6 +25,6 @@ public class MainController {
 //        return dataRepository.findAll();
 //    }
     public Data send(){
-        return new Data("Santhosh Kumar");
+        return new Data("Santhosh Kumar","santhoshfsdev@gmail.com",1234567890);
     }
 }
--- src/main/java/com/santhosh/repo/model/Data.java
+++ src/main/java/com/santhosh/repo/model/Data.java
@@ -5,8 +5,42 @@ import org.springframework.data.annotation.Id;
 public class Data {
 
     public String data;
+
+    public Data(String data, String email, int mobile) {
+        this.data = data;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    public String email;
+
+    public int mobile;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+
     
-    public Data(String data){
-        this.data = data; 
-}
+
 }
